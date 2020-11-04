5866c3c01690c0cd4c6d88b3a514cd293acbe4cb
ykurata <yasukokurata@gmail.com>
2020-11-04 17:37:50 -0500

    created video stream function

--- public/script.js
+++ public/script.js
@@ -1,9 +1,19 @@
 // connect to root path
 const socket = io("/");
+const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {  // Pass id === undefined because we generate an Id 
   host: '/',
   port: '3001',
 });
+const myVideo = document.createElement('video');
+myVideo.muted = true;  // Mute my own video
+
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {  // Promise. Pass stream
+  addVideoStream(myVideo, stream)
+})
 
 // Connect to this peer server.
 // As soon as we conect to peer server and get back on ID, we want to run this code.
@@ -15,4 +25,12 @@ myPeer.on('open', id => {
 // Listen to the event.
 socket.on('user-connected', userId => {
   console.log('User connected: ' + userId);
-})
+});
+
+function addVideoStream(video, stream) {
+  video.arcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  })
+  videoGrid.append(video);
+}
