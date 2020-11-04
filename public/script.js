95fbf620054ec0d8253ae8e3426d5b746cf50781
ykurata <yasukokurata@gmail.com>
2020-11-04 17:01:42 -0500

    added peer

--- public/script.js
+++ public/script.js
@@ -1,7 +1,13 @@
+// connect to root path
 const socket = io("/");
+const myPeer = new Peer(undefined, {
+  host: '/',
+  port: '3001',
+});
 
 socket.emit("join-room", ROOM_ID, 10);
 
+// Listen to the event.
 socket.on('user-connected', userId => {
   console.log('User connected: ' + userId);
 })
--- server.js
+++ server.js
@@ -24,7 +24,13 @@ app.get("/:room", (req, res) => {
 // and then it's call all the code inside socket.io
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
+    // Current socket to join the current room.
+    // Joining this new room that we passed with current user.
     socket.join(roomId);
+    // We are going to send a message to the room that we are currently in 
+    // Make sure this is a broadcast message.
+    // Send it to everyone else in the same room, but don't send it back to me.
+    // because we already know we connected.
     socket.to(roomId).broadcast.emit('user-connected', userId);
   })
 })
