23ad5c24ceaf527ef0dc2ca603feb5c837315709
ykurata <yasukokurata@gmail.com>
2020-11-04 17:12:05 -0500

    created a new peer

--- public/script.js
+++ public/script.js
@@ -1,11 +1,16 @@
 // connect to root path
 const socket = io("/");
-const myPeer = new Peer(undefined, {
+const myPeer = new Peer(undefined, {  // Pass id === undefined because we generate an Id 
   host: '/',
   port: '3001',
 });
 
-socket.emit("join-room", ROOM_ID, 10);
+// Connect to this peer server.
+// As soon as we conect to peer server and get back on ID, we want to run this code.
+
+myPeer.on('open', id => {
+  socket.emit("join-room", ROOM_ID, id);  // We pass the userId whenever we actually join
+});
 
 // Listen to the event.
 socket.on('user-connected', userId => {
