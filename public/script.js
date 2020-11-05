d0c92131e92acc065794b14bc867a73ae395a897
ykurata <yasukokurata@gmail.com>
2020-11-05 12:46:03 -0500

    updated the function to connect to other user

--- public/script.js
+++ public/script.js
@@ -13,6 +13,10 @@ navigator.mediaDevices.getUserMedia({
   audio: true
 }).then(stream => {  // Promise. Pass stream
   addVideoStream(myVideo, stream)
+  // Allow ourselves to connect to ohter users
+  socket.on('user-connnected', userId => {
+    connectToNewUser(userId, stream);
+  });
 })
 
 // Connect to this peer server.
@@ -22,15 +26,23 @@ myPeer.on('open', id => {
   socket.emit("join-room", ROOM_ID, id);  // We pass the userId whenever we actually join
 });
 
-// Listen to the event.
-socket.on('user-connected', userId => {
-  console.log('User connected: ' + userId);
-});
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call();
+  // Call a user that we give a certain ID. Sending the userId and sending them ouer video 
+  // and audio stream.
+  const video = document.createElement('video');
+  call.on('stream', userVideoStream => {   // Taking stream from other user we are calling,
+    addVideoStream(video, userVideoStream);  // and adding it into our own custom video element on our page.
+  });
+  call.on('close', () => {
+    video.remove();
+  })
+}
 
 function addVideoStream(video, stream) {
-  video.arcObject = stream;
+  video.srcObject = stream
   video.addEventListener('loadedmetadata', () => {
-    video.play();
+    video.play()
   })
-  videoGrid.append(video);
+  videoGrid.append(video)
 }
